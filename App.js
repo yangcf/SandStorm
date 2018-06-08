bd01627e08ae131b89a8f322ff0e9ded4b8b0146
yangcf <836843022@qq.com>
2018-06-08 17:19:53 +0800

    delete useless code

--- App.js
+++ App.js
@@ -11,7 +11,6 @@ import {
   Text,
   View,
 } from 'react-native';
-import { MapView } from 'react-native-amap3d'
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
@@ -20,64 +19,7 @@ const instructions = Platform.select({
 });
 
 export default class App extends Component {
-
-  // static navigationOptions = ({ navigation }) => {
-  //   const { state, setParams } = navigation
-  //   state.params = state.params || { mapType: 'standard' }
-  //   const props = {
-  //     mode: 'dropdown',
-  //     style: { width: 100 },
-  //     selectedValue: state.params.mapType,
-  //     onValueChange: mapType => setParams({ mapType }),
-  //   }
-  //   return {
-  //     title: '地图模式',
-  //     headerRight: (
-  //       <Picker {...props}>
-  //         <Picker.Item label="标准" value="standard" />
-  //         <Picker.Item label="卫星" value="satellite" />
-  //         <Picker.Item label="导航" value="navigation" />
-  //         <Picker.Item label="夜间" value="night" />
-  //         <Picker.Item label="公交" value="bus" />
-  //       </Picker>
-  //     ),
-  //   }
-  // }
-
   render() {
-    return (
-        <MapView
-          locationEnabled = {true}
-          distanceFilter={100}
-          onLocation={({ nativeEvent }) =>console.log(`${nativeEvent.latitude},${nativeEvent.longitude},${nativeEvent.timestamp},${nativeEvent.speed},${nativeEvent.accuracy}`)}
-          showsLocationButton
-          showsBuildings
-          showsCompass
-          showsZoomControls
-          zoomEnabled
-          scrollEnabled
-          rotateEnabled
-          tiltEnabled
-          mapType={'standard'}
-          showsScale
-          style={StyleSheet.absoluteFill}
-          locationType={'location_rotate_no_center'}
-          >
-            <MapView.Marker
-              draggable
-              active
-              color={'red'}
-              title='将我拖拽到公司地址~'
-              description = '请长按~'
-              onDragEnd={({ nativeEvent }) =>console.log(`${nativeEvent.latitude}, ${nativeEvent.longitude}`)}
-              coordinate={{
-                latitude: 39.955384086445804,
-                longitude: 116.33783608675003,
-              }}
-            />
-          </MapView>
-        
-    )
   }
 }
 
